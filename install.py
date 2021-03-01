6054ff717a0dbc7b88eda1ecfb841ba3346e592b
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2021-03-01 12:17:02 +0300

    Update install.py

--- install.py
+++ install.py
@@ -161,7 +161,7 @@ def target_is_writable(pth):
 
 def create_tmp_copy(pth):
     """ Create temporary copy of the file """
-    fn_tmp = tempfile._get_candidate_names()  # pylint: disable=W0212
+    fn_tmp = next(tempfile._get_candidate_names())  # pylint: disable=W0212
     fn_tmp = path.basename(pth) + '_' + fn_tmp.name
     copyf(pth, fn_tmp)
     return fn_tmp
